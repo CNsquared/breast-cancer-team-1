41b823f206334a1643d0d59ff114851abd6a702f
Christopher Brown <chrisrbrown15@gmail.com>
2025-04-23 01:24:29 -0700

    dummy main.py

--- project-2/main.py
+++ project-2/main.py
@@ -1,13 +1,12 @@
-from src.pipeline import preprocess
-from src.pipeline import run_analysis
-from src.pipeline import postprocess
+from src.pipeline import preprocess, run_analysis, postprocess
+
+def main():
+
+    data = preprocess.something()
+
+    results = run_analysis.something(data)
+
+    postprocess.something(results)
 
 if __name__ == "__main__":
-
-    print("Running pipeline")
-    print("Preprocessing data")
-    preprocess()
-    print("Running analysis")
-    run_analysis()
-    print("Postprocessing data")
-    postprocess()
+    main()
