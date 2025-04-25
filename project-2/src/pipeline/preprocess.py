739a3b58dd837a418fc9278d395fde56f3944400
Molly Sacks <msacks@ucsd.edu>
2025-04-25 08:46:14 -0700

    update-preprocess.py

--- project-2/src/pipeline/preprocess.py
+++ project-2/src/pipeline/preprocess.py
@@ -1,32 +1,37 @@
 import pandas as pd
 
-# read in raw mutation data
-df_mut = pd.read_csv('../../data/raw/TCGA.BRCA.mutations.txt', sep='\t')
 
-# exclude samples the didn't pass QC
-df_mut = df_mut[df_mut['FILTER'] == 'PASS']
+def preprocess():
+    # read in raw mutation data
+    df_mut = pd.read_csv('data/raw/TCGA.BRCA.mutations.txt', sep='\t')
 
-# exclude indels
-df_mut = df_mut[df_mut['Variant_Type'] == 'SNP']
+    # exclude samples the didn't pass QC
+    df_mut = df_mut[df_mut['FILTER'] == 'PASS']
 
-# define mutation type
-df_mut.loc[:,'mutation_type'] = df_mut.apply(lambda row: 'synonymous' if row['Variant_Classification'] == 'Silent' else 'non-synonymous' if row['CDS_position'] != '_' else None, axis=1)
+    # exclude indels
+    df_mut = df_mut[df_mut['Variant_Type'] == 'SNP']
 
-# remove genes that don't have a symbol (they all have symbols FYI)
-df_mut = df_mut[df_mut['Hugo_Symbol'].notna()]
+    # define mutation type
+    df_mut.loc[:,'mutation_type'] = df_mut.apply(lambda row: 'synonymous' if row['Variant_Classification'] == 'Silent' else 'non-synonymous' if row['CDS_position'] != '_' else None, axis=1)
 
-# remove rows without a positiion
-df_mut = df_mut[df_mut['Start_Position'].notna()]
+    # remove genes that don't have a symbol (they all have symbols FYI)
+    df_mut = df_mut[df_mut['Hugo_Symbol'].notna()]
 
-# filter out hypermutators
-# count mutations per patient
-mutation_counts = df_mut['patient_id'].value_counts()
+    # remove rows without a positiion
+    df_mut = df_mut[df_mut['Start_Position'].notna()]
 
-# keep only patients with <= 500 mutations
-normal_mutators = mutation_counts[mutation_counts <= 500].index
+    # filter out hypermutators
+    # count mutations per patient
+    mutation_counts = df_mut['patient_id'].value_counts()
 
-# filter out hypermutators
-df_mut = df_mut[df_mut['patient_id'].isin(normal_mutators)]
+    # keep only patients with <= 500 mutations
+    normal_mutators = mutation_counts[mutation_counts <= 500].index
 
-# write output
-df_mut.to_csv('../../data/processed/TCGA.BRCA.mutations.qc1.txt')
+    # filter out hypermutators
+    df_mut = df_mut[df_mut['patient_id'].isin(normal_mutators)]
+
+    # write output
+    #df_mut.to_csv('../../data/processed/TCGA.BRCA.mutations.qc1.txt')
+
+    # return output
+    return df_mut
