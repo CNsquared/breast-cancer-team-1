7e2ee3fe2cc5ceac949475873b3c8789000d007f
Christopher Brown <chrisrbrown15@gmail.com>
2025-04-27 22:00:11 -0700

    logging

--- project-2/main.py
+++ project-2/main.py
@@ -1,14 +1,18 @@
 from src.pipeline import preprocess, run_analysis, postprocess
 
+PROCESSED_OUTPUT= 'data/processed/TCGA.BRCA.mutations.qc1.txt'
+
 def main():
 
     print("Preprocesssing mutation data...")
     data = preprocess.preprocess()
-    data.to_csv('data/processed/TCGA.BRCA.mutations.qc1.txt')
+    data.to_csv(PROCESSED_OUTPUT)
+    print(f"Preprocessed data saved to {PROCESSED_OUTPUT}")
 
     #results = run_analysis.something(data)
 
     #postprocess.something(results)
+    print("Job completed successfully!")
 
 if __name__ == "__main__":
     main()
