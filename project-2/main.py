5159e358661316f14069346fcaf8e67bea5259a8
Molly Sacks <msacks@ucsd.edu>
2025-04-29 17:57:09 -0700

    add-skeleton-functions

--- project-2/main.py
+++ project-2/main.py
@@ -1,18 +1,23 @@
 from src.pipeline import preprocess, run_analysis, postprocess
 
-PROCESSED_OUTPUT= 'data/processed/TCGA.BRCA.mutations.qc1.txt'
-
 def main():
 
     print("Preprocesssing mutation data...")
-    data = preprocess.preprocess()
-    data.to_csv(PROCESSED_OUTPUT)
-    print(f"Preprocessed data saved to {PROCESSED_OUTPUT}")
+    mutations_processed = preprocess.preprocess_mutations()
+    
+    print("Preprocessing reference data...")
+    reference_processed = preprocess.preprocess_reference()
 
-    #results = run_analysis.something(data)
+    print("Counting observed synonymous and non-synonymous SNVs")
+    counts_df = run_analysis.count_mutations(mutations_processed)
+
+    print("Calculating possible synonymous and non-synonymous SNVs")
+    df_sizes = run_analysis.calculate_possible_mutations(reference_processed)
+
+    print("Calculating dN/dS and generating p-values")
+    results = run_analysis.evaluate(counts_df, df_sizes)
 
     #postprocess.something(results)
-    print("Job completed successfully!")
 
 if __name__ == "__main__":
     main()
