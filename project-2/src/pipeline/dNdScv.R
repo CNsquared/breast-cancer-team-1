67b5c3609b1520d05b8142fafb366f544cc92991
Zaid <Zaid@me.com>
2025-05-02 19:16:51 -0700

    Initial commit

---
+++ project-2/src/pipeline/dNdScv.R
@@ -0,0 +1,29 @@
+#Install libraries
+
+install.packages('devtools')
+install.packages('tidyverse')
+
+library(devtools)
+install_github("im3sanger/dndscv")
+library(tidyverse)
+library(dndscv)
+
+#Load mutations 
+Mutations <- read.table('./project-2/data/processed/TCGA.BRCA.mutations.qc1.txt',header = T)
+
+#Select and rename them for dNdScv
+Mutations_sub <- Mutations %>% select(patient_id,Chromosome,Start_Position,Reference_Allele,Allele)
+colnames(Mutations_sub) <- c('sampleID','chr','pos','ref','mut')
+
+#Run dNdScv
+dndsout = dndscv(Mutations_sub)
+
+sel_cv = dndsout$sel_cv
+
+#Print top 10 genes in statistical significance
+print('Top 10 genes in statistical significance')
+print(head(sel_cv,10), digits = 3)
+
+#Export dNdScv results as csv file 
+print('Export dNdScv results as csv file')
+write.csv(sel_cv,'./project-2/data/processed/dNdScv_output.csv')
