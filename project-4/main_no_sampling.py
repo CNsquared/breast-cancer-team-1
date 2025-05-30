1fbd9f596769c39a7da3092f44fc73dba546f5b7
Christopher Brown <chrisrbrown15@gmail.com>
2025-05-29 23:13:26 -0700

    datetime stamp for model

--- project-4/main_no_sampling.py
+++ project-4/main_no_sampling.py
@@ -6,63 +6,75 @@ from src.utils.data_loader import load_expression_matrix
 from src.utils.sampling import SamplingRunner
 import numpy as np
 import pandas as pd
+import torch
+import os
 
-import argparse
+import datetime
 
-def parse_args():
-    parser = argparse.ArgumentParser()  
-    
-    parser.add_argument('--latent_dim', type=int, default=15, help='Number of latent features')
-    parser.add_argument('--hidden_dim', type=int, nargs='+', default=[128, 64],
-                        help='Hidden dimensions in encoder, e.g., --hidden_dim 128 64')
-    parser.add_argument('--lr', type=float, default=5e-4, help='Learning rate')
-    parser.add_argument('--batch_size', type=int, default=16, help='Batch size')
-    parser.add_argument('--max_epochs', type=int, default=200, help='Maximum epochs')
-    parser.add_argument('--patience', type=int, default=10, help='Number of epocs where model training saturated')
+geneExpParams = {
+    'top_N': 250,
+    'subset_method': 'dndscv'    # method to subset genes {dndscv, variance}
+}
 
-    parser.add_argument('--top_N', type=int, default=250, help="Top 'n' genes used for analysis")
-    parser.add_argument('--subset_method', type=str, default='dndscv', help="Method to select top 'n' genes")
+autoEncoderParams = {
+    'latent_dim': 5,            # Number of latent features
+    'hidden_dims': [128,64],    # Hidden layers
+    'lr': 5e-4,                 # Learning rate
+    'dropout_rate': 0.2,        # dropout rate for the autoencoder
+    'weight_decay': 1e-5        # Weight decay for the optimizer (L2 regularization)
+}
 
-    return parser.parse_args()
-    
+trainingParams = {
+    'patience': 10,            # Number of epochs where model training saturated
+    'max_epochs': 200,         # Maximum epochs
+    'delta': 1e-4,            # Minimum change in validation loss to consider improvement
+}
+
 def main():
-    args = parse_args()
 
-    all_exp = GeneExpPreprocessor(top_N=args.top_N,
-            subset_method=args.subset_method, filter_subtypes=False).get_df()
-    
-    basal_samples = GeneExpPreprocessor(top_N=args.top_N,subset_method=args.subset_method, filter_subtypes=True).get_df().index
-
+    # get expression data
+    all_exp = GeneExpPreprocessor(**geneExpParams, filter_subtypes=False).get_df()
+    basal_samples = GeneExpPreprocessor(**geneExpParams).get_df().index
+    basal_exp = all_exp.loc[basal_samples]
     print(f"Expression data shape (train data): {all_exp.shape}")
-    gene_names = all_exp.columns
     
-    runner = GeneExpressionRunner(all_exp,latent_dim=args.latent_dim,
-        hidden_dims=args.hidden_dim,
-        lr=args.lr,
-        batch_size=args.batch_size)
+    # create runner for autoencoder
+    runner = GeneExpressionRunner(all_exp, **autoEncoderParams)
+    
+    # run cross validation
     cv_losses = runner.cross_validate()
     print(f'cv_losses: {cv_losses}')
 
-    # train on all samples and get latent space
-    basal_exp = all_exp.loc[basal_samples]
-    model, scaler, weights_all = runner.train_all_and_encode(return_model=True)
-    latent = runner.trained_model_encode(model, basal_exp, scaler)
+    # train on all samples and get model, scaler
+    model, scaler, weights_all = runner.train_all_and_encode(**trainingParams, return_model=True)
+
+    # use model and scaler to encode subtypes
+    print("Running subtype-specific latent_space extraction...")
+    subtypes= ['BRCA_LumA', 'BRCA_Her2', 'BRCA_LumB', 'BRCA_Normal', 'BRCA_Basal']
+    for subtype in subtypes:
+        subtype_samples = GeneExpPreprocessor(**geneExpParams, subtypes=[subtype]).get_df().index
+        subtype_exp = all_exp.loc[subtype_samples]
+        print(f"{subtype} Expression data shape: {subtype_exp.shape}")
+        latent = runner.trained_model_encode(model, subtype_exp, scaler)
+
+        # train on all samples and get latent space
+        df_latent = pd.DataFrame(latent, index=subtype_exp.index, columns=[f"latent_{i}" for i in range(latent.shape[1])])
+        df_latent.to_csv(f"results/tables/latent_space_{autoEncoderParams['latent_dim']}dim_{subtype}.csv")
+        print("Done extracting latent space for subtype:", subtype)
 
     # write out latent space, weights, cv losses, and pretrained model
-
     print("Saving results...")
 
-    # save model
+    os.makedirs("results/models", exist_ok=True)
+    datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    torch.save(model.state_dict(), f"results/models/no_sampling_autoencoder_{datetime}.pth")  # save model state
 
-
-    df_latent = pd.DataFrame(latent, index=basal_exp.index, columns=[f"latent_{i}" for i in range(latent.shape[1])])
-    df_latent.to_csv(f"results/tables/no_sampling_latent_space.csv")
-
-    weights_all = pd.DataFrame(weights_all, index=gene_names)
+    weights_all = pd.DataFrame(weights_all, index=all_exp.columns)
     weights_all.to_csv(f"results/tables/no_sampling_gene_to_latent_weights.csv")
     
     cv_losses_df = pd.DataFrame(cv_losses, index=[f"fold_{i+1}" for i in range(len(cv_losses))])
     cv_losses_df.to_csv(f"results/tables/no_sampling_cv_losses.csv", index=False, header=False)
+    print("Job completed successfully!")
 
 if __name__ == "__main__":
     main()
